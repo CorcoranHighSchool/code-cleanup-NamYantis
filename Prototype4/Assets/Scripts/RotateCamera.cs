66ef81ec0eae908f871e9b9c1312a30ae053414e
NamYantis <142627513+NamYantis@users.noreply.github.com>
2024-04-10 13:24:19 -0700

    Removed Unused Libraries, Removed Empty Methods, Serialize Public Properties, Converted Strings into Const. Optimized Vector3 Math RotateCamera.cs

--- Prototype4/Assets/Scripts/RotateCamera.cs
+++ Prototype4/Assets/Scripts/RotateCamera.cs
@@ -1,21 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class RotateCamera : MonoBehaviour
 {
-    public float rotationSpeed;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    [Serialize Field] private float rotationSpeed;
+    private const string Horizontal = "Horizontal";
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
+        float horizontalInput = Input.GetAxis(Horizontal);
+        transform.Rotate(Vector3.up, horizontalInput * (rotationSpeed * Time.deltaTime));
     }
 }
