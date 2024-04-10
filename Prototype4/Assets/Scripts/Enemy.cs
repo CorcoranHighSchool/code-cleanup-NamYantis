e6d24b274ce255013ac1d79cc5df6bb3707c3d17
NamYantis <142627513+NamYantis@users.noreply.github.com>
2024-04-10 13:10:03 -0700

    Removed Unused Libraries, Serialized Public Properties, Converted Strings to Const. Enemy.cs

--- Prototype4/Assets/Scripts/Enemy.cs
+++ Prototype4/Assets/Scripts/Enemy.cs
@@ -1,17 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public float speed = 3.0f;
+    [Serialize Field] private float speed = 3.0f;
     private Rigidbody enemyRb;
     private GameObject player;
+    private const string Player = "Player";
 
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.Find("Player");
+        player = GameObject.Find(Player);
         enemyRb = GetComponent<Rigidbody>();
     }
 
